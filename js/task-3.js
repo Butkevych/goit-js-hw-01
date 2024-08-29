5461542e7859c1bf77807b5ff299242af0c0df48
Butkevych <104171010+Butkevych@users.noreply.github.com>
2024-08-29 13:16:05 +0300

    fixed some poroblems

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,11 @@
 function getElementWidth(content, padding, border) {
   const boxSize =
-    Number.parseInt(content) +
-    Number.parseInt(padding) * 2 +
+    Number.parseFloat(content) +
+    Number.parseFloat(padding) * 2 +
     Number.parseFloat(border) * 2;
   return boxSize;
 }
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
+getElementWidth();
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
